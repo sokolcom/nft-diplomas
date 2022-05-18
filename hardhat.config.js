a33794878592f9a314b42f8ab5d432f88a251107
sokolcom <suget169@gmail.com>
2022-05-19 01:20:48 +0300

    [ADD]: script to deploy factory to a chain

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,28 @@
+require("@nomiclabs/hardhat-waffle");
+require('hardhat-docgen');
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// Substrate predefined key (for testing)
+const priv_key = "99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342";
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.2",
+  networks: {
+    frontier: {
+      url: `http://localhost:9933`,
+      accounts: [priv_key]
+    }
+  },
 };
---
+++ scripts/deployFactory.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const hre = require("hardhat");
+
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile 
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  const OrganizationFactory = await hre.ethers.getContractFactory("OrganizationFactory");
+  const organizationFactory = await OrganizationFactory.deploy();
+  await organizationFactory.deployed();
+
+  console.log("LetterFactory deployed to:", organizationFactory.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
