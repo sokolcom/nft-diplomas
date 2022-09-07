95d0ad5e136a1c3023fae3d34cdafb2caa76fd5a
sokolcom <suget169@gmail.com>
2022-09-07 17:50:44 +0300

    [ADD]: paperwork  srcs

---
+++ docs/code/Organization.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts-upgradeable/ token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/ access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/ access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+
+contract Organization is ERC721Upgradeable, OwnableUpgradeable, AccessControlUpgradeable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _diplomaIds;
+    string private _organizationName;
+    uint private constant MAX_NAME_LENGTH = 0xFF;
+    bool private _open;
+
+    mapping(uint256 => string) private _diplomaURIs;
+
+    bytes32 private constant READER_ROLE = keccak256("READER_ROLE");  // AccessControl role
+
+    event OrganizationCreated(address owner, string organizationName);
+    event DiplomaMinted(address owner, string organizationName, uint256 diplomaId);
+
+    function initOrganization(string memory organizationName, address _owner)
+        public
+        initializer
+    {
+        __ERC721_init("Organization", "ORG");
+        __Ownable_init();
+
+        require(bytes(organizationName).length <= MAX_NAME_LENGTH, "Organization name's too long!");
+        _organizationName = organizationName;
+
+        _grantRole(DEFAULT_ADMIN_ROLE, _owner);
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        grantRole(READER_ROLE, _owner);
+        transferOwnership(_owner);
+        emit OrganizationCreated(_owner, organizationName);
+    }
+
+    modifier onlyReader() {
+        if (!_open) {
+            require(hasRole(READER_ROLE, msg.sender), "Restricted to <READER> role only!");
+        }
+        _;
+    }
+
+    function isReader(address account) public view returns (bool) {
+        if (_open) {
+            return true;
+        }
+        return hasRole(READER_ROLE, account);
+    }
+
+    function addReader(address account) public virtual onlyOwner {
+        grantRole(READER_ROLE, account);
+    }
+
+    function removeReader(address account) public virtual onlyOwner {
+        revokeRole(READER_ROLE, account);
+    }
+
+    function open() public virtual onlyOwner {
+        _open = true;
+    }
+
+    function close() public virtual onlyOwner {
+        _open = false;
+    }
+
+    // ---------------------------
+    // View functions ------------
+    // ---------------------------
+
+    function getOrganizationName() public view onlyReader returns (string memory) {
+        return _organizationName;
+    }
+
+    function getDiploma(uint256 idx) public view onlyReader returns (string memory) {
+        require((idx <= _diplomaIds.current()), "Index out of range!");
+        return _diplomaURIs[idx];
+    }
+
+    function getDiplomaAmount() public view onlyReader returns (uint256) {
+        return _diplomaIds.current();
+    }
+
+    function isOpen() public view returns (bool) {
+        return _open;
+    }
+
+    // Imposed by OpenZeppelin...
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(AccessControlUpgradeable, ERC721Upgradeable)
+        returns (bool)
+    {
+        return
+            interfaceId == type(IAccessControlUpgradeable).interfaceId ||
+            interfaceId == type(IERC721Upgradeable).interfaceId ||
+            interfaceId == type(IERC721MetadataUpgradeable).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+
+    // ---------------------------
+    // Mint functions ------------
+    // ---------------------------
+
+    function mintDiploma(address recipient, string memory diplomaURI) public onlyOwner returns (uint256) {
+        require(bytes(diplomaURI).length > 0, "Cannot mint empty diploma!");
+
+        _diplomaIds.increment();
+        uint256 newDiplomaId = _diplomaIds.current();
+
+        _mint(recipient, newDiplomaId);
+        _diplomaURIs[newDiplomaId] = diplomaURI;
+        // _setTokenURI(newDiplomaId, diplomaURI);
+
+        emit DiplomaMinted(recipient, _organizationName, newDiplomaId);
+
+        return newDiplomaId;
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId)
+        public
+        virtual
+        override
+    {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
+
+        _transfer(from, to, tokenId);    
+        _grantRole(READER_ROLE, to);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data)
+        public
+        virtual
+        override
+    {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
+
+        _safeTransfer(from, to, tokenId, _data);
+        _grantRole(READER_ROLE, to);
+    }
+}
---
+++ docs/code/node.rs
@@ -0,0 +1,91 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![recursion_limit = "256"]
+
+
+#[cfg(feature = "std")]
+include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
+
+
+use pallet_grandpa::{
+	fg_primitives, AuthorityId as GrandpaId, AuthorityList as GrandpaAuthorityList,
+};
+use pallet_ethereum::{Call::transact, Transaction as EthereumTransaction};
+use pallet_evm::{Account as EVMAccount, EnsureAddressTruncated, HashedAddressMapping, Runner};
+use pallet_evm::FeeCalculator;
+
+
+parameter_types! {
+	pub const MaxAuthorities: u32 = 100;
+}
+
+impl pallet_aura::Config for Runtime {
+	type AuthorityId = AuraId;
+	type DisabledValidators = ();
+	type MaxAuthorities = MaxAuthorities;
+}
+
+impl pallet_grandpa::Config for Runtime {
+	type Event = Event;
+	type Call = Call;
+
+	type KeyOwnerProofSystem = ();
+
+	type KeyOwnerProof =
+		<Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(KeyTypeId, GrandpaId)>>::Proof;
+
+	type KeyOwnerIdentification = <Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(
+		KeyTypeId,
+		GrandpaId,
+	)>>::IdentificationTuple;
+
+	type HandleEquivocation = ();
+
+	type WeightInfo = ();
+	type MaxAuthorities = ConstU32<32>;
+}
+
+parameter_types! {
+	pub const ChainId: u64 = 100;
+	pub BlockGasLimit: U256 = U256::from(u32::max_value());
+	pub PrecompilesValue: FrontierPrecompiles<Runtime> = FrontierPrecompiles::<_>::new();
+}
+
+impl pallet_evm::Config for Runtime {
+	type FeeCalculator = BaseFee;
+	type GasWeightMapping = ();
+	type BlockHashMapping = pallet_ethereum::EthereumBlockHashMapping<Self>;
+	type CallOrigin = EnsureAddressTruncated;
+	type WithdrawOrigin = EnsureAddressTruncated;
+	type AddressMapping = HashedAddressMapping<BlakeTwo256>;
+	type Currency = Balances;
+	type Event = Event;
+	type Runner = pallet_evm::runner::stack::Runner<Self>;
+	type PrecompilesType = FrontierPrecompiles<Self>;
+	type PrecompilesValue = PrecompilesValue;
+	type ChainId = ChainId;
+	type BlockGasLimit = BlockGasLimit;
+	type OnChargeTransaction = ();
+	type FindAuthor = FindAuthorTruncated<Aura>;
+}
+
+impl pallet_ethereum::Config for Runtime {
+	type Event = Event;
+	type StateRoot = pallet_ethereum::IntermediateStateRoot<Self>;
+}
+
+construct_runtime!(
+	pub enum Runtime where
+		Block = Block,
+		NodeBlock = opaque::Block,
+		UncheckedExtrinsic = UncheckedExtrinsic
+	{
+		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+		// ...
+		Aura: pallet_aura::{Pallet, Config<T>},
+		Grandpa: pallet_grandpa::{Pallet, Call, Storage, Config, Event},
+		// ...
+		Ethereum: pallet_ethereum::{Pallet, Call, Storage, Event, Config, Origin},
+		EVM: pallet_evm::{Pallet, Config, Call, Storage, Event<T>},
+		// ...
+	}
+);
